36c66437ed4607c1770f020145919ad0e84e6d1b
natashalysakova <natashalysakova@gmail.com>
2025-08-13 09:25:37 +0300

    Add project files.

---
+++ MangaShelf.Api.Tests/Test1.cs
@@ -0,0 +1,38 @@
+﻿using MangaShelf.Api.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace MangaShelf.Api.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            // This method is called before each test method.
+        }
+
+        [TestMethod]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true, "This test should always pass.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AlwaysFail()
+        {
+            Assert.IsFalse(true, "This test should always fail.");
+        }
+
+        [TestMethod]
+        public void XheckTheWeatther()
+        {
+            var looger = new LoggerFactory().CreateLogger<WeatherForecastController>();
+            WeatherForecastController controller = new WeatherForecastController(looger);
+            var result = controller.Get();
+
+            Assert.IsNotNull(result, "The result should not be null.");
+        }
+    }
+}
---
+++ MangaShelf.Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿namespace MangaShelf.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            // This method is called before each test method.
+        }
+
+        [TestMethod]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true, "This test should always pass.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AlwaysFail()
+        {
+            Assert.IsFalse(true, "This test should always fail.");
+        }
+    }
+}
