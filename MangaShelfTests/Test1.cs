df6d74699e3794c9ced09d6e81022723bb5682a1
natashalysakova <natashalysakova@gmail.com>
2025-08-14 22:00:35 +0300

    Add tests project

---
+++ MangaShelfTests/Test1.cs
@@ -0,0 +1,23 @@
+﻿namespace MangaShelfTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            // This method is called before each test method.
+        }
+
+        [TestMethod]
+        public void AlwaysPass()
+        {
+            Assert.IsTrue(true, "This test should always pass.");
+        }
+        [TestMethod]
+        public void AlwaysFail()
+        {
+            Assert.IsFalse(false, "This test should always fail.");
+        }
+    }
+}
