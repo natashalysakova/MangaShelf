6da770c665ffb10f528641d7d91ec5f5af02b9fe
natashalysakova <natashalysakova@gmail.com>
2026-08-18 00:56:17 +0300

    add index

---
+++ MangaShelf.DAL/Migrations/20260817215558_AddIndex.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaShelf.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Users",
+                type: "varchar(255)",
+                nullable: false,
+                collation: "utf8mb4_unicode_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("Relational:Collation", "utf8mb4_unicode_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdentityUserId",
+                table: "Users",
+                column: "IdentityUserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IdentityUserId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IdentityUserId",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_unicode_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .OldAnnotation("Relational:Collation", "utf8mb4_unicode_ci");
+        }
+    }
+}
